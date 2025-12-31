e606d543b9b9a9317656a33f6dbbaf1dfe1303d5
VxASI <chirag.awale1@gmail.com>
2025-12-31 00:01:36 -0500

    init

--- ios/PolyphonicIOS/AppDelegate.mm
+++ ios/PolyphonicIOS/AppDelegate.mm
@@ -17,7 +17,8 @@
 - (NSURL *)sourceURLForBridge:(RCTBridge *)bridge
 {
 #if DEBUG
-  return [[RCTBundleURLProvider sharedSettings] jsBundleURLForBundleRoot:@"index"];
+  return [NSURL URLWithString:@"http://localhost:8081/index.bundle?platform=ios&dev=true"];
+  // return [[RCTBundleURLProvider sharedSettings] jsBundleURLForBundleRoot:@"index"];
 #else
   return [[NSBundle mainBundle] URLForResource:@"main" withExtension:@"jsbundle"];
 #endif
